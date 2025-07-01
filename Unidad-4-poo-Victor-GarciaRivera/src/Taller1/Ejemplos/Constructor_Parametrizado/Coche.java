4779c6271813520f39856983c171d3297741dd37
Victor <victo.elcatire75@gmail.com>
2025-06-30 20:57:03 -0500

    feat: Victor Garcia - ejemplo 3 taller 1

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller1/Ejemplos/Constructor_Parametrizado/Coche.java
@@ -0,0 +1,12 @@
+package Taller1.Ejemplos.Constructor_Parametrizado;
+
+// Se usa para inicializar propiedades con valores proporcionados por el usuario.
+
+public class Coche {
+    String marca;
+
+    // Constructor parametrizado
+    public Coche(String marca) {
+        this.marca = marca;
+    }
+}
