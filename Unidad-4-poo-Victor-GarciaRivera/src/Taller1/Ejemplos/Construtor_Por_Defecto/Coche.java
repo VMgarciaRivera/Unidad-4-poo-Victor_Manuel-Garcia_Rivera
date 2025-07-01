d9f2c2a484ee8839ba7de8ea135b5ec0dfe5343c
Victor <victo.elcatire75@gmail.com>
2025-06-30 20:56:24 -0500

    feat: Victor Garcia - ejemplo 2 taller 1

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller1/Ejemplos/Construtor_Por_Defecto/Coche.java
@@ -0,0 +1,12 @@
+package Taller1.Ejemplos.Construtor_Por_Defecto;
+
+//Es un constructor sin parámetros que inicializa las propiedades con valores predeterminados
+
+public class Coche {
+    String marca;
+
+    // Constructor por defecto
+    public Coche() {
+        marca = "Desconocida";
+    }
+}
