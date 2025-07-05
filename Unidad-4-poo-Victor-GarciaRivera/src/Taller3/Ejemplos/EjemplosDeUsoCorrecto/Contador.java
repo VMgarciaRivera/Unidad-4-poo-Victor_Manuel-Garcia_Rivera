d50028a7799c9cf1b07b2b2aac9de0035d30d798
Victor <victo.elcatire75@gmail.com>
2025-07-04 20:59:35 -0500

    feat: Victor Garcia - ejemplo1 Talle3

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller3/Ejemplos/EjemplosDeUsoCorrecto/Contador.java
@@ -0,0 +1,26 @@
+package Taller3.Ejemplos.EjemplosDeUsoCorrecto;
+
+// EjemploCorrecto1:AtributostaticCompartidoentreInstancias
+
+public class Contador {
+    static int contadorGlobal = 0; // Atributo estático
+
+    public Contador() {
+        contadorGlobal++; // Incrementa el contador global cada vez que se crea un objeto
+    }
+
+    public static void mostrarContador() {
+        System.out.println("Contador global: " + contadorGlobal);
+    }
+
+
+    public class Main {
+        public static void main(String[] args) {
+            Contador c1 = new Contador();
+            Contador c2 = new Contador();
+            Contador c3 = new Contador();
+            // Se accede al método estático usando el nombre de la clase
+            Contador.mostrarContador(); // Muestra: Contador global: 3
+        }
+    }
+}
