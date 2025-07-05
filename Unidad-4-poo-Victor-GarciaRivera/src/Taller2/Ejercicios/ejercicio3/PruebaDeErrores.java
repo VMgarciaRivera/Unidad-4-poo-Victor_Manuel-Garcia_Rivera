52626633c1732d1a46fa258c948d6a1c22612e05
Victor <victo.elcatire75@gmail.com>
2025-07-04 20:30:44 -0500

    feat: Victor Garcia - ejercicio 3 con errores de compilacion Taller2

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller2/Ejercicios/ejercicio3/PruebaDeErrores.java
@@ -0,0 +1,9 @@
+package Taller2.Ejercicios.ejercicio3;
+
+public class PruebaDeErrores {
+    private String error;
+
+    public static void mostrarError(){
+        System.out.println(this.error);
+    }
+}
