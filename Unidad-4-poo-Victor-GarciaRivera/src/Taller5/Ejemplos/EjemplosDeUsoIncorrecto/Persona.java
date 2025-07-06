c9de39bb138b6062b7aad8c88628770709f79584
Victor <victo.elcatire75@gmail.com>
2025-07-06 15:44:21 -0500

    feat: Victo Garcia - ejemplo3 Taller5

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller5/Ejemplos/EjemplosDeUsoIncorrecto/Persona.java
@@ -0,0 +1,19 @@
+package Taller5.Ejemplos.EjemplosDeUsoIncorrecto;
+
+
+// Ejemplo Incorrecto 2: No utilizar encapsulamiento adecuado
+// Clase Producto con nivel de acceso de paquete
+class Producto {
+    String nombre; // Atributo de paquete (default)
+    double precio; // Atributo de paquete (default)
+
+    Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+
+    void mostrarInfo() {
+        System.out.println("Nombre: " + nombre + ", Precio: " + precio);
+    }
+}
