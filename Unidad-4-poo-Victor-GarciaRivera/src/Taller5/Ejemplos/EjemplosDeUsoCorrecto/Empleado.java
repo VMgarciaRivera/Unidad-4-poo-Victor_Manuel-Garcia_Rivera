195ee65756798a5d2ebe9207b773adbbea8b3063
Victor <victo.elcatire75@gmail.com>
2025-07-06 15:34:22 -0500

    feat: Victor Garcia -ejemplo1 Taller5

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller5/Ejemplos/EjemplosDeUsoCorrecto/Empleado.java
@@ -0,0 +1,14 @@
+package Taller5.Ejemplos.EjemplosDeUsoCorrecto;
+
+// Clase con miembros de paquete (default)
+class Empleado {
+    String nombre;
+    double salario;
+    Empleado(String nombre, double salario) {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+    void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre + ", Salario: " + salario);
+    }
+}
