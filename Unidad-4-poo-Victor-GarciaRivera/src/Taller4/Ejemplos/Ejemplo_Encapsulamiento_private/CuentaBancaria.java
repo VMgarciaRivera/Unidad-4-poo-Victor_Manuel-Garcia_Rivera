e9c33597d25c8162f5103b500cbbe9c6698c9bee
Victor <victo.elcatire75@gmail.com>
2025-07-06 14:18:38 -0500

    feat: Victor Manuel - ejemplo1 Taller4

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller4/Ejemplos/Ejemplo_Encapsulamiento_private/CuentaBancaria.java
@@ -0,0 +1,23 @@
+package Taller4.Ejemplos.Ejemplo_Encapsulamiento_private;
+
+public class CuentaBancaria {
+    private double saldo; // Atributo privado para proteger el saldo
+
+    public CuentaBancaria(double saldoInicial) {
+        if (saldoInicial >= 0) {
+            this.saldo = saldoInicial;
+        } else {
+            this.saldo = 0;
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double monto) {
+        if(monto > 0) {
+            saldo += monto;
+        }
+    }
+}
