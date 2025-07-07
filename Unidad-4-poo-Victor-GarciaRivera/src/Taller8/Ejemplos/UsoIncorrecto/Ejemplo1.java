4462054e68f11514d374b60ba4850cfd4ff650cb
Victor <victo.elcatire75@gmail.com>
2025-07-07 10:20:59 -0500

    feat: Victor Garcia - ejemploUsoIncorrecto1 Taller8

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller8/Ejemplos/UsoIncorrecto/Ejemplo1.java
@@ -0,0 +1,11 @@
+package Taller8.Ejemplos.UsoIncorrecto;
+
+// Clase base 1
+class ClaseA {
+}
+// Clase base 2
+class ClaseB {
+}
+// Clase derivada (incorrecta)
+//class ClaseC extends ClaseA, ClaseB { // Error de compilación
+//    // No se puede heredar de múltiples clases en Java
