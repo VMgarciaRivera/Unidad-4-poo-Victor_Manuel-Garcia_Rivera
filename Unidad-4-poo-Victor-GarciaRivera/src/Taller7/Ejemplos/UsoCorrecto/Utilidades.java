2db93723b887d1078ab482c0294fefb7ca774fb7
Victor <victo.elcatire75@gmail.com>
2025-07-07 09:23:56 -0500

    feat: Victor Garcia - ejemploUsoCorrecto2 Taller 7

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller7/Ejemplos/UsoCorrecto/Utilidades.java
@@ -0,0 +1,10 @@
+package Taller7.Ejemplos.UsoCorrecto;
+
+// Explicación: La clase Utilidades define un método público y estática imprimirMensaje,
+// que puede ser accedido desde
+// cualquier otra clase.
+public class Utilidades {
+    public static void imprimirMensaje(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
