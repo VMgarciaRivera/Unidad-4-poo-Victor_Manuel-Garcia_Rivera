06fe1377d6d3949c56a365bca9d52be8c6bd1196
Victor <victo.elcatire75@gmail.com>
2025-07-07 17:04:17 -0500

    feat: Victor Garcia - ejemploIncorrecto1 Taller12

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller12/Ejemplos/UsoIncorrecto/NoImplementarMetodos.java
@@ -0,0 +1,10 @@
+package Taller12.Ejemplos.UsoIncorrecto;
+
+// Interfaz Volador
+interface Volador {
+    void volar();
+}
+// Clase incorrecta que no implementa todos los métodos
+//class Avion implements Volador {
+    // Error de compilación: La clase Avion debe implementar el método volar()
+//}
