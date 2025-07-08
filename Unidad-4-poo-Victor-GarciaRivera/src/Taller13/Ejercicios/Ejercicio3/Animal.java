8ea99e6000a17fe608b8d80a9fb3d667e51cab8c
Victor <victo.elcatire75@gmail.com>
2025-07-07 19:45:55 -0500

    feat: Victor Garcia - ejercicio3 Taller13

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller13/Ejercicios/Ejercicio3/Animal.java
@@ -0,0 +1,13 @@
+package Taller13.Ejercicios.Ejercicio3;
+
+public abstract class Animal {
+    // Se intenta definir "default" como en interfaces, lo cual es incorrecto
+   // public default void hacerSonido() {
+ //       System.out.println("Sonido de animal");
+   // }
+}
+//Error: illegal combination of modifiers: abstract and default
+
+//En clases abstractas, no se usa default,
+// simplemente defines el método normalmente (puede tener implementación).
+
---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller13/Ejercicios/Ejercicio3/Pajaro.java
@@ -0,0 +1,10 @@
+package Taller13.Ejercicios.Ejercicio3;
+
+interface Volador {
+    void volar();
+}
+
+
+//public class Pajaro implements Volador {
+    // ERROR: Pajaro is not abstract and does not override abstract method volar() in Volador
+//}
