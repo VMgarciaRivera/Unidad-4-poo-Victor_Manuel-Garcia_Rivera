3c82ce4bd3b0f2bfc9341b4129bacd6a482dbf3a
Victor <victo.elcatire75@gmail.com>
2025-07-07 20:41:43 -0500

    feat: Victor Garcia - ejemplo3 Taller15

---
+++ Unidad-4-poo-Victor-GarciaRivera/src/Taller15/Ejemplos/SistemasDeNotificaciones/EmailService.java
@@ -0,0 +1,9 @@
+package Taller15.Ejemplos.SistemasDeNotificaciones;
+
+// Clase EmailService (responsable de las notificaciones por correo)
+public class EmailService {
+    public void enviarEmail(String destinatario, String mensaje) {
+        System.out.println("Enviando email a: " + destinatario);
+        System.out.println("Mensaje: " + mensaje);
+    }
+}
